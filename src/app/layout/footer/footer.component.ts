dbe3abbc8e29fd8c9728cb9c81eda71c1f75d8ea
GIBB <GIBB@V-sgibb.UAB.local>
2022-11-15 18:02:50 -0300

    se ajusta footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -6,9 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-  constructor() { }
+
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
   }
+
 
 }
